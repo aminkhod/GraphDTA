330a3f6617e000b097949ac330ce62efe8f29e77
fsonya88 <falahati.sonya@gmail.com>
2024-02-04 21:08:52 +0330

    new GIN

--- models/ginconv.py
+++ models/ginconv.py
@@ -2,8 +2,59 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn import Sequential, Linear, ReLU
-from torch_geometric.nn import GINConv, global_add_pool
+from torch_geometric.nn import GINConv, global_add_pool,GATv2Conv,GATConv
 from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
+
+class AttentionBlock(nn.Module):
+    """ A class for attention mechanisn with QKV attention """
+
+    def __init__(self, hid_dim, n_heads, dropout):
+        super().__init__()
+
+        self.hid_dim = hid_dim
+        self.n_heads = n_heads
+
+        assert hid_dim % n_heads == 0
+
+        self.f_q = nn.Linear(hid_dim, hid_dim)
+        self.f_k = nn.Linear(hid_dim, hid_dim)
+        self.f_v = nn.Linear(hid_dim, hid_dim)
+
+        self.fc = nn.Linear(hid_dim, hid_dim)
+
+        self.do = nn.Dropout(dropout)
+
+        self.scale = torch.sqrt(torch.FloatTensor([hid_dim // n_heads])).cuda()
+
+    def forward(self, query, key, value, mask=None):
+        batch_size = query.shape[0]
+
+        Q = self.f_q(query)
+        K = self.f_k(key)
+        V = self.f_v(value)
+
+        Q = Q.view(batch_size, self.n_heads, self.hid_dim // self.n_heads).unsqueeze(3)
+        K_T = K.view(batch_size, self.n_heads, self.hid_dim // self.n_heads).unsqueeze(3).transpose(2, 3)
+        V = V.view(batch_size, self.n_heads, self.hid_dim // self.n_heads).unsqueeze(3)
+
+        energy = torch.matmul(Q, K_T) / self.scale
+
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, -1e10)
+
+        attention = self.do(F.softmax(energy, dim=-1))
+
+        weighter_matrix = torch.matmul(attention, V)
+
+        weighter_matrix = weighter_matrix.permute(0, 2, 1, 3).contiguous()
+
+        weighter_matrix = weighter_matrix.view(batch_size, self.n_heads * (self.hid_dim // self.n_heads))
+
+        weighter_matrix = self.do(self.fc(weighter_matrix))
+
+        return weighter_matrix
+
+
 
 # GINConv model
 class GINConvNet(torch.nn.Module):
@@ -16,8 +67,12 @@ class GINConvNet(torch.nn.Module):
         self.dropout = nn.Dropout(dropout)
         self.relu = nn.ReLU()
         self.n_output = n_output
+        
+        ##GAT 
+        self.gat = GATConv(num_features_xd, dim, heads=1)
+        
         # convolution layers
-        nn1 = Sequential(Linear(num_features_xd, dim), ReLU(), Linear(dim, dim))
+        nn1 = Sequential(Linear(dim, dim), ReLU(), Linear(dim, dim))
         self.conv1 = GINConv(nn1)
         self.bn1 = torch.nn.BatchNorm1d(dim)
 
@@ -37,7 +92,7 @@ class GINConvNet(torch.nn.Module):
         self.conv5 = GINConv(nn5)
         self.bn5 = torch.nn.BatchNorm1d(dim)
 
-        self.fc1_xd = Linear(dim, output_dim)
+        self.fc1_xd = Linear(dim, 128)
 
         # 1D convolution on protein sequence
         self.embedding_xt = nn.Embedding(num_features_xt + 1, embed_dim)
@@ -45,13 +100,42 @@ class GINConvNet(torch.nn.Module):
         self.fc1_xt = nn.Linear(32*121, output_dim)
 
         # combined layers
-        self.fc1 = nn.Linear(256, 1024)
-        self.fc2 = nn.Linear(1024, 256)
-        self.out = nn.Linear(256, self.n_output)        # n_output = 1 for regression task
+       # self.fc1 = nn.Linear(256, 1024)
+        #self.fc2 = nn.Linear(1024, 256)
+        #self.out = nn.Linear(256, self.n_output)        # n_output = 1 for regression task
+
+
+
+
+              # cross attention
+        self.att = AttentionBlock(128, 1, 0.2)          
+        
+           # output
+        self.fc = nn.Sequential(
+            nn.Linear(384, 1024),
+            nn.ReLU(),
+            nn.Dropout(),
+
+           # nn.Linear(1024, 1024),
+           # nn.ReLU(),
+           # nn.Dropout(),
+
+            nn.Linear(1024, 256),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(256, 1)
+        )
+
+        
+        
 
     def forward(self, data):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         target = data.target
+        
+        
+
+        x = F.relu(self.gat(x, edge_index)) # Apply GATConv layer
 
         x = F.relu(self.conv1(x, edge_index))
         x = self.bn1(x)
@@ -66,21 +150,33 @@ class GINConvNet(torch.nn.Module):
         x = global_add_pool(x, batch)
         x = F.relu(self.fc1_xd(x))
         x = F.dropout(x, p=0.2, training=self.training)
-
+        #x = self.relu(self.fc_g1(x))
+        
+        
         embedded_xt = self.embedding_xt(target)
         conv_xt = self.conv_xt_1(embedded_xt)
         # flatten
         xt = conv_xt.view(-1, 32 * 121)
         xt = self.fc1_xt(xt)
 
-        # concat
-        xc = torch.cat((x, xt), 1)
+        
+        # cross attention
+        att = self.att(x, xt, xt)
+
+        
+        ## concat
+        xc = torch.cat((x,att, xt), 1)
+        
         # add some dense layers
-        xc = self.fc1(xc)
-        xc = self.relu(xc)
-        xc = self.dropout(xc)
-        xc = self.fc2(xc)
-        xc = self.relu(xc)
-        xc = self.dropout(xc)
-        out = self.out(xc)
+        #xc = self.fc1(xc)
+        #xc = self.relu(xc)
+        #xc = self.dropout(xc)
+        #xc = self.fc2(xc)
+        #xc = self.relu(xc)
+        #xc = self.dropout(xc)
+        out = self.fc(xc)    
         return out
+        
+model = GINConvNet()
+print(model)        
+        
